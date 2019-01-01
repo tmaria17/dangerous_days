4835fb54d9459490fdb83ca30b56e5c50bb8f4eb
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-31 21:18:35 -0700

    Fix syntax error.

--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -1,18 +1,17 @@
 class NasaService
-
-
   def get_neos
-    get_json("neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=#{ENV['NASA_KEY']}")
+    get_json("/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07")[:near_earth_objects]
   end
   private
-    def get_json
-      JSON.parse(conn.get(url).body, symbolize_names: true)
-    end
-
     def conn
       url = "https://api.nasa.gov"
       Faraday.new(url: url ) do |faraday|
-        faraday.adapter Faraday.deafault_adapter
+        faraday.params["api_key"] = ENV["NASA_KEY"]
+        faraday.adapter Faraday.default_adapter
       end
+    end
+
+    def get_json(url)
+      JSON.parse(conn.get(url).body, symbolize_names: true)
     end
 end
