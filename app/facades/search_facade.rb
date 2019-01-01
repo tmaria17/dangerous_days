fad01082cfba5234a86880bacf79e55a6cf2ec2f
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-31 22:11:46 -0700

    Add method to make neo array.

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -3,19 +3,29 @@ class SearchFacade
   def initialize(params)
     @start_date = params["start_date"].to_s.to_datetime.strftime("%B %-d, %Y")
     @end_date = params["end_date"].to_s.to_datetime.strftime("%B %-d, %Y")
+    @most_dangerous_day = most_dangerous_day.to_s.to_datetime.strftime("%B %-d, %Y")
+  end
+
+  def neos
+    space_rocks = []
+    neos_data[most_dangerous_day].each do |space_rock_data|
+     space_rocks.push(Neo.new(space_rock_data)) if space_rock_data[:is_potentially_hazardous_asteroid]
+    end
+    # binding.pry
+    space_rocks
   end
 
   def most_dangerous_day
     hash = Hash.new(0)
-    neos.each do |neo|
+    neos_data.each do |neo|
       neo[1].each do |space|
          hash[neo[0]] += 1 if space[:is_potentially_hazardous_asteroid]
       end
      end
-    hash.max_by{|k,v| v}.first.to_s.to_datetime.strftime("%B %-d, %Y")
+    hash.max_by{|k,v| v}.first
   end
 
-  def neos
+  def neos_data
     service.get_neos
   end
 
